4384ad4dd28cf5d0994194884f2c79ed8ef6573c
Zach Goldstein <zachgold@gmail.com>
2023-03-29 16:26:02 -0400

    Add actual uid generating logic instead of using uid lib

--- maelstrom-2-uid/go.mod
+++ maelstrom-2-uid/go.mod
@@ -3,5 +3,3 @@ module maelstrom-2-uid
 go 1.19
 
 require github.com/jepsen-io/maelstrom/demo/go v0.0.0-20230228144258-93f15782f204
-
-require github.com/google/uuid v1.3.0 // indirect
--- maelstrom-2-uid/main.go
+++ maelstrom-2-uid/main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"crypto/rand"
 	"encoding/json"
+	"fmt"
 	"log"
+	"math/big"
+	"time"
 
-	"github.com/google/uuid"
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+// generateUID returns a uid, based on the unix nanosecond time and a secure random number
 func generateUID() string {
-	id := uuid.New()
-	return id.String()
+	timeNs := time.Now().UnixNano()
+	c := 10
+	b := make([]byte, c)
+	_, err := rand.Read(b)
+	if err != nil {
+		fmt.Println("error generating UID:", err)
+		return ""
+	}
+	return fmt.Sprintf("%v-%v", int(big.NewInt(0).SetBytes(b).Uint64()), timeNs)
 }
 
 type generateMsgReq struct {
